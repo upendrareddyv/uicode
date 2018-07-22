75657fb73fbfb650a8982cec7bd7bf9a12c6e5e7
Durgaprasad <prasaddurga244@gmail.com>
2018-07-22 10:52:48 +0530

    server changes

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,15 +3,30 @@
  */
 var express = require('express');
 var router = express.Router();
+var mongojs = require("mongojs"),
+    config = require('config.json'),
+    db = mongojs(config.connectionString);
+
 
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works')
 });
+router.get('/excess_category_based_on_time', function(req, res, next) {
+    
+    db.collection('excess_category_based_on_time').find({}, function(error, data) {
+        if (error) {
+            res.json([]);
 
-var db = require("../../queries");
+        } else {
+            res.json({ status: 'true', data: data });
+        }
+    });
+});
+
+/*var db = require("../../queries");
 router.get('/records', db.getAllPuppies);
 router.get('/results', db.getSinglePuppy);
 router.get('/res', db.getResults);
-router.get('/arbor', db.getArbor);
+router.get('/arbor', db.getArbor);*/
 module.exports = router;
